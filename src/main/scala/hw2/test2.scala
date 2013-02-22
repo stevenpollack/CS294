d8807649fc3f43e07bbfaf69bda7d59bba0d0421
Steven Pollack <steven@pollackphoto.net>
2013-02-21 19:31:13 -0800

    made a bullshit class

--- src/main/scala/hw2/test2.scala
+++ src/main/scala/hw2/test2.scala
@@ -1,1 +1,5 @@
-// some scala code.
+class person(height: Float, weight: Float) {
+	override def toString() = {
+		println("person's height is "+this.height+" and their weight is "+this.weight)
+	}
+}
