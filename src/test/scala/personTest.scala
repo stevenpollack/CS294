21b1242e65cdcca715ad6d1eb2856c627dff10cf
Steven Pollack <steven@pollackphoto.net>
2013-02-22 00:49:13 -0800

    updated personTest to test out scalatest features

--- src/test/scala/personTest.scala
+++ src/test/scala/personTest.scala
@@ -1,5 +1,7 @@
 import testobjects.Person
 import org.scalatest._
+import org.scalatest.matchers._
+import org.scalatest.prop._
 
 class PersonSpec extends FlatSpec {
 	intercept[IllegalArgumentException] {
@@ -16,4 +18,59 @@ class PersonSpec extends FlatSpec {
 		testPerson.first
 	}
 	
+}
+
+class SomeOtherTest extends FreeSpec with PropertyChecks with ShouldMatchers {
+	"Simple Integers" - {
+		"should exhibit associative behavior" - {
+			"additive associativity:" in {
+				forAll { (a: Int, b: Int, c: Int) =>
+					((a+b)+c) should be (a+(b+c))
+				}	
+			}
+			"multiplicative associativity:" in {
+				forAll {(a: Int, b: Int, c: Int) =>
+					((a*b)*c) should be (a*(b*c))
+				}
+			}
+		}
+
+		"should exhibit commutativity" - {
+			"additive associativity:" in {
+				forAll { (a: Int, b: Int) =>
+					(a+b) should be (b+a)	
+				}
+			}
+			"multiplicative associativity:" in {
+				pending
+			}
+		}
+
+		"have that (-a^2)*(-b^2) > 0" - {
+			forAll{ (a: Int, b: Int) =>
+				whenever (a < 0 && b < 0) {
+					val product: Double = (a toDouble)*(b toDouble)
+					product should be >= (0.0)
+				}
+
+			}
+		}
+	}
+}
+
+class SomeTest extends FeatureSpec with ShouldMatchers with GivenWhenThen {
+	feature ("ShouldMatcher provide nice error messages"){
+	     scenario ("Making a correct assertion"){
+		      given ("a number")
+		      val number = 5
+		      and ("an other number")
+		      val otherNumber = 23
+		      when ("multiplying one with the other")
+		      val result = number * otherNumber
+		      and ("the other way round")
+		      val otherResult = otherNumber * number
+		      then ("the results will be the same")
+		      result should equal (otherResult)
+	    }
+	}
 }
